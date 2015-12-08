5c0d3b81be3ac5750c883ca611b82c6762af5449
Bigmac91 <makey223@gmail.com>
2015-12-08 15:53:02 +0000

    Create perfloghome.php

---
+++ perfloghome.php
@@ -0,0 +1,12 @@
+<?php
+    function logToPerformace($msg)
+    {
+        //open the txt file with append privileges
+        $myfile = fopen("logs/homelog.txt", "a") or die ("Unable to open file!");
+        // prepare the log message in a string with timestamp and clients IP address
+        $txt = date ('Y-m-d H:i:s') . ":" . $msg . "(" . $_SERVER['REMOTE_ADDR'] . ")\n";
+        fwrite ($myfile, $txt);
+        //close the txt file
+        fclose ($myfile);    
+    }
+?>
