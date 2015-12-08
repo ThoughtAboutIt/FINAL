330c8a2e1b5631292e5f4ad497e7247047755d54
Bigmac91 <makey223@gmail.com>
2015-12-08 15:53:32 +0000

    Update home.php

--- home.php
+++ home.php
@@ -1,24 +1,28 @@
-<?php
-session_start();
-include 'includes/dbconnect.php';
-if(!isset($_SESSION['user']))
-{
-	header("Location: index.php");
-}
-$res=mysqli_query($conn,"SELECT Email FROM users WHERE user_id=".$_SESSION['user']);
-while($row = mysqli_fetch_array($res))
-{
-    $_SESSION['username'] = $row['Email'];
-}
-?>
-
-
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 <title>Welcome<?php echo $userRow['email']; ?></title>
 <link rel="stylesheet" href="css/style.css" type="text/css" />
+    <?php 
+    
+        global $startdate;
+        global $enddate;
+        $startdate=microtime(true);
+     
+        session_start();
+        include_once "includes/dbconnect.php";
+        include_once "includes/perfloghome.php";
+        if(!isset($_SESSION['user']))
+        {
+	       header("Location: index.php");
+        }
+        $res=mysqli_query($conn,"SELECT Email FROM users WHERE user_id=".$_SESSION['user']);
+        while($row = mysqli_fetch_array($res))
+        {
+            $_SESSION['username'] = $row['Email'];
+        }
+    ?>
 </head>
 <body>
 <div id="header">
@@ -30,5 +34,10 @@ while($row = mysqli_fetch_array($res))
         </div>
     </div>
 </div>
+    <?php
+    $enddate=microtime(true);
+    $interval=round(($enddate-$startdate)*1000,2);
+    logToPerformace("Home Page loaded in " . $interval . "ms ");
+?>
 </body>
 </html>
